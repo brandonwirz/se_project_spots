f16ee5c7110fed11f96c828c144c6e4e6755f026
brandonwirz <bwirz75@gmail.com>
2025-12-04 22:27:55 -0700

    modal functions, update readme

--- scripts/index.js
+++ scripts/index.js
@@ -45,7 +45,7 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-
+modal;
 const editPostForm = newPostModal.querySelector(".modal__form");
 const postForm = document.forms.editPostForm;
 
